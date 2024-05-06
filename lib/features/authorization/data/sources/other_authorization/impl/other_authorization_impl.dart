83818eae25152cf6e899340b0a737c2510a153e5
sarbador <avaz.shams.2002@gmail.com>
2024-05-06 23:16:47 +0500

    uploaded

--- lib/features/authorization/data/sources/other_authorization/impl/other_authorization_impl.dart
+++ lib/features/authorization/data/sources/other_authorization/impl/other_authorization_impl.dart
@@ -1,12 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:yahay/core/app_settings/dio/app_http_routes.dart';
+import 'package:yahay/core/app_settings/dio/dio_settings.dart';
+import 'package:yahay/core/app_settings/dio/http_status_codes.dart';
 import 'package:yahay/core/global_data/models/user_model/user_model.dart';
 import 'package:yahay/features/authorization/data/sources/other_authorization/other_authorization.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:yahay/injections/injections.dart';
 
 class OtherAuthorizationImpl implements OtherAuthorization {
+  final DioSettings _dioSettings = snoopy<DioSettings>();
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FacebookAuth _facebookAuth = FacebookAuth.instance;
+
+  final String _googleAuthPath = "/google-auth";
 
   @override
   Future<UserModel?> faceBookAuth() async {
@@ -23,11 +32,29 @@ class OtherAuthorizationImpl implements OtherAuthorization {
   @override
   Future<UserModel?> googleAuth() async {
     try {
-      final data = await _googleSignIn.signIn();
+      final googleAccount = await _googleSignIn.signIn();
 
-      debugPrint("google auth user: ${data?.email} | ${data?.id}");
+      if (googleAccount == null) return null;
 
-      if (data == null) return null;
+      final body = {
+        "email": googleAccount.email,
+        "google_id": googleAccount.id,
+      };
+
+      final url = "${AppHttpRoutes.authPrefix}$_googleAuthPath";
+
+      final response = await _dioSettings.dio.post(url, data: body);
+
+      debugPrint("google auth laravel response: ${response.data}");
+
+      if (response.statusCode != HttpStatusCodes.success) return null;
+
+      Map<String, dynamic> json =
+          response.data is String ? jsonDecode(response.data) : response.data;
+
+      if (!json.containsKey(HttpStatusCodes.serverSuccessResponse)) {
+        return null;
+      }
     } catch (e) {
       debugPrint("google auth error is: $e");
       return null;
