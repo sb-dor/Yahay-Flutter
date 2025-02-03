f2b78eb0feaf6effb1762123f4781b8572b4e335
sb-dor <avaz.shams.2002@gmail.com>
2025-02-03 09:18:52 +0500

    writing code for chat message stream transformer

--- lib/src/features/chat_screen/bloc/stream_transformers/chat_message_stream_transformer.dart
+++ lib/src/features/chat_screen/bloc/stream_transformers/chat_message_stream_transformer.dart
@@ -1,10 +1,19 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:dart_pusher_channels/dart_pusher_channels.dart';
+import 'package:logger/logger.dart';
 import 'package:yahay/src/core/models/chat_message_model/chat_message_model.dart';
+import 'package:yahay/src/core/models/chats_model/chat_model.dart';
 
+// TODO: write code that returns or chatMessage or chat itself
 final class ChatMessageStreamTransformer
     extends StreamTransformerBase<ChannelReadEvent, ChatMessageModel> {
+  //
+  ChatMessageStreamTransformer({required final Logger logger}) : _logger = logger;
+
+  final Logger _logger;
+
   @override
   Stream<ChatMessageModel> bind(Stream<ChannelReadEvent> stream) {
     StreamSubscription? subscription;
@@ -17,6 +26,38 @@ final class ChatMessageStreamTransformer
 
     subscription = stream.listen(
       (event) {
+        Map<String, dynamic> messageJson = jsonDecode(event.data ?? '');
+
+        if (messageJson.containsKey('message') && messageJson['message'] != null) {
+          ChatMessageModel message =
+              ChatMessageModel.fromJson(messageJson['message']).copyWith(messageSent: true);
+          currentStateModel = _addMessage(
+            message: message,
+            currentStateModel: currentStateModel,
+          );
+        }
+
+        // find problem here
+        if (messageJson.containsKey('chat') && messageJson['chat'] != null) {
+          _logger.log(
+            Level.debug,
+            "chat has chat room: ${currentStateModel.currentChat?.videoChatRoom}",
+          );
+          ChatModel chat = ChatModel.fromJson(messageJson['chat']);
+          currentStateModel = _setChat(
+            chat: currentStateModel.currentChat?.copyWith(
+              videoChatRoom: chat.videoChatRoom,
+              videoChatStreaming: chat.videoChatStreaming,
+            ),
+            setChatMessages: false,
+            currentStateModel: currentStateModel,
+          );
+          _logger.log(
+            Level.debug,
+            "chat has chat room 2: ${currentStateModel.currentChat?.videoChatRoom}",
+          );
+        }
+
         controller.add(
           ChatMessageModel(),
         );
